0b62c6cf478a88640eb95ff8c69a689cc5c5eb6f
builam66 <builam66@users.noreply.github.com>
2021-01-23 04:31:15 +0700

    Avatar 2
    Up anh len duoc roi

--- ecUcademy42/src/main/java/models/userModel.java
+++ ecUcademy42/src/main/java/models/userModel.java
@@ -1,4 +1,17 @@
 package models;
 
+import beans.course;
+import beans.user;
+import org.sql2o.Connection;
+import utils.dbUtils;
+
+import java.util.List;
+
 public class userModel {
+    public static List<user> getUserByIdAndUsername(){
+        String sql = "select * from user where user_id=:user_id and user_name=:user_name";
+        try(Connection con = dbUtils.getConnection()){
+            return con.createQuery(sql).executeAndFetch(user.class);
+        }
+    }
 }
